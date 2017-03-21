14797f9f14a6981c5c7c679d65a1c9ff0a726f22
Helder De Oliveira <hemarque@gmail.com>
2017-03-21 21:29:07 +0100

    first failing test

---
+++ test/fizzbuzz.js
@@ -0,0 +1,11 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+var assert = require('assert');
+
+describe('FizzBuzz kata', function(){
+	it('should return 1 when 1', function(){
+		assert(fizzbuzz(1), 1);
+	});
+});
